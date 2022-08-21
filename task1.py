98d3d93d3ac3ea0a8f8c9227c6daa4317add2f79
shree <shreyashmohod@gmail.com>
2022-08-21 15:31:47 +0530

    1

--- task1.py
+++ task1.py
@@ -4,35 +4,25 @@
 # 4 . Write a program to fetch a record via api
 # 5 . All the above questions you have to answer for mongo db as well .
 
-import mysql.connector as conn
-
-mydb = conn.connect(host = "localhost" , user ="root" , passwd = "haonabhai" )
-cursor = mydb.cursor()
-
 from flask import Flask, request, jsonify
+import mysql.connector as conn
 
 app = Flask(__name__)
 
-@app.route('/abc',methods=['GET','POST'])
-def test1():
-    if(request.method=='POST'):
-        a = request.json['a']
-        b = request.json['b']
-        c = request.json['c']
-        d = request.json['d']
-        e = request.json['e']
-        cursor.execute("insert into shree.ineuron values(a,b,c,d,e)")
-        mydb.commit()
-        q2 = cursor.execute("select * from sudhanshu.ineuron1")
-        return jsonify((q2))
-
-if __name__=='__main__':
-    app.run()
+mydb = conn.connect(host = "localhost" , user ="root" , passwd = "haonabhai" )
+cursor = mydb.cursor()
+cursor.execute("create database if not exists shree")
+cursor.execute("create table if not exists shree.ineuron(name varchar(30), number int)")
 
 
 
-# "a":101,
-#     "b":'sudhanshu kumar',
-#     "c":'sudhanshu@ineuron.ai',
-#     "d": 100,
-#     "e":30
+# @app.route('/abc',methods=['GET','POST'])
+# def test1():
+#     if(request.method=='POST'):
+#
+#
+# if __name__=='__main__':
+#     app.run()
+
+
+
